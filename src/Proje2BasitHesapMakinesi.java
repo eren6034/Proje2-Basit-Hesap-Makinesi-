05e585bd13c9acb65bfee490dc19f8b8689117e1
ErenCanErdoğan <107648911+eren6034@users.noreply.github.com>
2024-01-13 16:32:26 +0300

    Update Proje2BasitHesapMakinesi.java

--- src/Proje2BasitHesapMakinesi.java
+++ src/Proje2BasitHesapMakinesi.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Proje2BasitHesapMakinesi {
+import java.util.Scanner;
+
+public class Proje2BasitHesapMakinesi {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("İki sayı ve işlemi girin Toplama(+) Çıkarma(-) Çarpma(*) Bölme(/) (örneğin-> 5 + 3):");
+        double sonuc = 0.0;
+
+
+        String ifade = scanner.nextLine();
+
+
+        String[] parcalar = ifade.split(" ");
+
+        double sayi1 = Double.parseDouble(parcalar[0]);
+        char operator = parcalar[1].charAt(0);
+        double sayi2 = Double.parseDouble(parcalar[2]);
+
+
+        switch (operator) {
+            case '+':
+                sonuc = sayi1 + sayi2;
+                break;
+            case '-':
+                sonuc = sayi1 - sayi2;
+                break;
+            case '*':
+                sonuc = sayi1 * sayi2;
+                break;
+            case '/':
+                if (sayi2 != 0) {
+                    sonuc = sayi1 / sayi2;
+                } else {
+                    System.out.println("Hata! Sıfıra bölme hatası.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Geçersiz İşlem Lütfen +, -, *, / operatörlerinden birini kullanın.");
+                return;
+        }
+
+        System.out.println("Sonuç: " + sonuc);
+        scanner.close();
+    }
 }
